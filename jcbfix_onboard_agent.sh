dd83786de38ff36861ff38cfcbba394a69584e9d
JoelCBennett <35112555+JoelCBennett@users.noreply.github.com>
2018-01-04 11:00:55 -0800

    fix path

--- jcbfix_onboard_agent.sh
+++ jcbfix_onboard_agent.sh
@@ -6,7 +6,7 @@
 
 
 # Values to be updated upon each new release
-GITHUB_RELEASE="https://github.com/Microsoft/OMS-Agent-for-Linux/releases/tag/OMSAgent_GA_v1.4.2-125"
+GITHUB_RELEASE="https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.4.2-125/"
 BUNDLE_X64="omsagent-1.4.2-125.universal.x64.sh"
 BUNDLE_X86="omsagent-1.4.2-125.universal.x86.sh"
 
